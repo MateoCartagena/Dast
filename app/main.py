1618f3568d3056d6eed2026691d38be003cf9ee9
MateoCartagena <mateo.cartagena@udla.edu.ec>
2025-06-25 13:52:33 -0500

    Pry

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ def suma(a, b):
 
 if __name__ == "__main__":
     import sys
-    a = int(sys.argv[1])
-    b = int(sys.argv[2])
+    exec("a = " + sys.argv[1])  # Uso inseguro de exec
+    exec("b = " + sys.argv[2])  # Uso inseguro de exec
     print(f"Resultado: {suma(a, b)}")
